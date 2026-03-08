1da0c7e674b89d5bfb2f6c492d647d5dde348d55
maulidyaashfraa-png <maulidyaashfraa@gmail.com>
2026-03-08 23:23:13 +0700

    added:Rumus Hitung Luas Belah Ketupat

--- rumus.py
+++ rumus.py
@@ -7,12 +7,15 @@ def hitung_luas_persegi(panjang_sisi):
 def hitung_luas_lingkaran(jari_jari):
     return 3.14 * jari_jari ** 2
 
-def hitung_luas(menu, *args):
+def hitung_luas_belah_ketupat(diagonal1, diagonal2):
+    return 0.5 * diagonal1 * diagonal2 hitung_luas(menu, *args):
     if menu == 1:
         return hitung_luas_segitiga(*args)
     elif menu == 2:
         return hitung_luas_persegi(*args)
     elif menu == 3:
         return hitung_luas_lingkaran(*args)
+    elif menu == 4:
+        return hitung_luas_belah_ketupat(*args)
     else:
         return "Menu tidak valid"
